e305ecc1732f3fddfab602c5100e378dd24d47c2
garvit <garvitmaloo.21@gmail.com>
2023-01-24 16:25:20 +0530

    Basic Project Setup

---
+++ pages/_document.jsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="/icon.png" type="image/png" />
+        <script
+          async
+          src="https://kit.fontawesome.com/e8be5a49bb.js"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import classes from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next JS starting template</title>
+      </Head>
+      <main></main>
+    </div>
+  );
+}
